zcomputerwiz/sparev2-blockchain

use run_generator2 rust call and compact spend bundle conditions data structure pervasively.

chia/types/spend_bundle_conditions.py
@@ -1,0 +1,29 @@
+from typing import List, Optional, Tuple
+from dataclasses import dataclass
+from chia.types.blockchain_format.sized_bytes import bytes32, bytes48
+from chia.util.ints import uint64, uint32
+from chia.util.streamable import Streamable, streamable
+
+
+# the Spend and SpendBundleConditions classes are mirrors of native types, returned by
+# run_generator2
+@dataclass(frozen=True)
+@streamable
+class Spend(Streamable):
+    coin_id: bytes32
+    puzzle_hash: bytes32
+    height_relative: Optional[uint32]
+    seconds_relative: uint64
+    create_coin: List[Tuple[bytes32, uint64, bytes]]
+    agg_sig_me: List[Tuple[bytes48, bytes]]
+
+
+@dataclass(frozen=True)
+@streamable
+class SpendBundleConditions(Streamable):
+    spends: List[Spend]
+    reserve_fee: uint64
+    height_absolute: uint32
+    seconds_absolute: uint64
+    agg_sig_unsafe: List[Tuple[bytes48, bytes]]
+    cost: uint64

RightSexyOrc/chia-blockchain

chia/types/spend_bundle_conditions.py
@@ -1,0 +1,30 @@
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+
+from chia.types.blockchain_format.sized_bytes import bytes32, bytes48
+from chia.util.ints import uint32, uint64
+from chia.util.streamable import Streamable, streamable
+
+
+# the Spend and SpendBundleConditions classes are mirrors of native types, returned by
+# run_generator2
+@dataclass(frozen=True)
+@streamable
+class Spend(Streamable):
+    coin_id: bytes32
+    puzzle_hash: bytes32
+    height_relative: Optional[uint32]
+    seconds_relative: uint64
+    create_coin: List[Tuple[bytes32, uint64, bytes]]
+    agg_sig_me: List[Tuple[bytes48, bytes]]
+
+
+@dataclass(frozen=True)
+@streamable
+class SpendBundleConditions(Streamable):
+    spends: List[Spend]
+    reserve_fee: uint64
+    height_absolute: uint32
+    seconds_absolute: uint64
+    agg_sig_unsafe: List[Tuple[bytes48, bytes]]
+    cost: uint64